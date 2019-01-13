31abc5c1aef7ee8780e7c83cc01bebc1701b5092
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 04:53:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name}. You are number "
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
 end
   
