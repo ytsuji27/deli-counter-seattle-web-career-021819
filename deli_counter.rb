bee7366566bb3ef564beab97eb4e7c2a87e50f23
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 04:48:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.count == 0
     puts "The line is currently empty."
   else
-    names_string = []
+    names_string = ""
     array.each.with_index do |name, number|
       names_string.push("#{number+1}. #{name}")
     end
