fd5381ff87300da2b4c59bcacb55f853a3a6f07f
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 04:49:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(array)
   else
     names_string = ""
     array.each.with_index do |name, number|
-      names_string.concat("#{number+1}. #{name}")
+      names_string.concat(" #{number+1}. #{name}")
     end
-    puts "The line is currently: #{names_string}"
+    puts "The line is currently:#{names_string}"
   end
 end
 
